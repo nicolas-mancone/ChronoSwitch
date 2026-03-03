68b76a96a46ff9dbdd19652e4718e6677b506983
seaglasses14 <luca.degennaro0302@gmail.com>
2026-03-03 05:20:32 +0100

    Added Host/Join functionalities (WIP)
    - Added basic SessionsWidget to test
    - Successfully create sessions and transports player to map
    - Joining is untested (requires 2 devices with separate steam accounts)
    
    Previous commit:
    - Fixed ChronalAnchorGrid

--- Source/ChronoSwitch/Private/Game/ChronoSwitchGameInstance.cpp
+++ Source/ChronoSwitch/Private/Game/ChronoSwitchGameInstance.cpp
@@ -2,3 +2,100 @@
 
 
 #include "ChronoSwitch/Public/Game/ChronoSwitchGameInstance.h"
+
+#include "OnlineSessionSettings.h"
+#include "OnlineSubsystemUtils.h"
+
+// To Do: Test if GetWorld() can be reliably used in GameInstance
+
+UChronoSwitchGameInstance::UChronoSwitchGameInstance() { }
+
+void UChronoSwitchGameInstance::Init()
+{
+	Super::Init();
+	
+	if (IOnlineSubsystem* OnlineSubsystem = Online::GetSubsystem(GetWorld()))
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Found Subsystem: %s"), *OnlineSubsystem->GetSubsystemName().ToString());
+		SessionInterface = OnlineSubsystem->GetSessionInterface();
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Subsystem Not Found"));
+	}
+}
+
+#pragma region Host Join Logic
+
+void UChronoSwitchGameInstance::HostSession(int32 MaxPlayers)
+{
+	if (!SessionInterface.IsValid()) return;
+	GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Yellow, TEXT("Host Joining Logic"));
+	FOnlineSessionSettings SessionSettings;
+	SessionSettings.bIsLANMatch = false;
+	SessionSettings.NumPublicConnections = MaxPlayers;
+	SessionSettings.bAllowJoinInProgress = true;
+	SessionSettings.bAllowJoinViaPresence = true;
+	SessionSettings.bShouldAdvertise = true;
+	SessionSettings.bUsesPresence = true;
+	SessionSettings.bUseLobbiesIfAvailable = true;
+	// This functionalities should help with using steam functionalities such as Friends Invites
+
+	SessionInterface->AddOnCreateSessionCompleteDelegate_Handle(FOnCreateSessionCompleteDelegate::CreateUObject(this, &UChronoSwitchGameInstance::OnCreateSessionComplete));
+	SessionInterface->CreateSession(0, NAME_GameSession, SessionSettings);
+}
+
+void UChronoSwitchGameInstance::FindJoinSession()
+{
+	if (!SessionInterface.IsValid()) return;
+
+	SessionSearch = MakeShareable(new FOnlineSessionSearch());
+	SessionSearch->MaxSearchResults = 10; // We should need less
+	SessionSearch->QuerySettings.Set(SEARCH_PRESENCE, true, EOnlineComparisonOp::Equals);
+
+	SessionInterface->AddOnFindSessionsCompleteDelegate_Handle(FOnFindSessionsCompleteDelegate::CreateUObject(this, &UChronoSwitchGameInstance::OnFindSessionsComplete));
+	SessionInterface->FindSessions(0, SessionSearch.ToSharedRef());
+}
+
+#pragma endregion
+
+#pragma region Delegates
+
+void UChronoSwitchGameInstance::OnCreateSessionComplete(FName SessionName, bool bWasSuccessful)
+{
+	GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Yellow, TEXT("OnSessionCreated"));
+	if (bWasSuccessful)
+	{
+		GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Yellow, TEXT("OnSessionCreatedSuccess"));
+		// Maybe wait for other player
+		// Travel to map as a Listen Server
+		GetWorld()->ServerTravel("/Game/ChronoSwitch/Testing/Levels/L_GameplayTest?listen");
+	}
+}
+
+void UChronoSwitchGameInstance::OnFindSessionsComplete(bool bWasSuccessful)
+{
+	if (bWasSuccessful && SessionSearch->SearchResults.Num() > 0)
+	{
+		SessionInterface->AddOnJoinSessionCompleteDelegate_Handle(FOnJoinSessionCompleteDelegate::CreateUObject(this, &UChronoSwitchGameInstance::OnJoinSessionComplete));
+		//Need to use specific Session name
+		SessionInterface->JoinSession(0, NAME_GameSession, SessionSearch->SearchResults[0]);
+	}
+}
+
+void UChronoSwitchGameInstance::OnJoinSessionComplete(FName SessionName, EOnJoinSessionCompleteResult::Type Result)
+{
+	if (Result == EOnJoinSessionCompleteResult::Success)
+	{
+		if (APlayerController* PC = GetFirstLocalPlayerController())
+		{
+			FString JoinURL;
+			if (SessionInterface->GetResolvedConnectString(SessionName, JoinURL))
+			{
+				PC->ClientTravel(JoinURL, ETravelType::TRAVEL_Absolute);
+			}
+		}
+	}
+}
+
+#pragma endregion
--- Source/ChronoSwitch/Public/Game/ChronoSwitchGameInstance.h
+++ Source/ChronoSwitch/Public/Game/ChronoSwitchGameInstance.h
@@ -4,7 +4,11 @@
 
 #include "CoreMinimal.h"
 #include "Engine/GameInstance.h"
+#include "Interfaces/OnlineSessionInterface.h"
 #include "ChronoSwitchGameInstance.generated.h"
+
+// Search Settings Key Defines
+#define SEARCH_PRESENCE FName(TEXT("PRESENCESEARCH"))
 
 /**
  * 
@@ -15,4 +19,21 @@ class CHRONOSWITCH_API UChronoSwitchGameInstance : public UGameInstance
 	GENERATED_BODY()
 	
 public:
+	UChronoSwitchGameInstance();
+	
+	virtual void Init() override;
+	
+	UFUNCTION(BlueprintCallable)
+	void HostSession(int32 MaxPlayers = 2);
+
+	UFUNCTION(BlueprintCallable)
+	void FindJoinSession();
+	
+protected:
+	void OnCreateSessionComplete(FName SessionName, bool bWasSuccessful);
+	void OnFindSessionsComplete(bool bWasSuccessful);
+	void OnJoinSessionComplete(FName SessionName, EOnJoinSessionCompleteResult::Type Result);
+
+	IOnlineSessionPtr SessionInterface;
+	TSharedPtr<FOnlineSessionSearch> SessionSearch;
 };
