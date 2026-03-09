f81649968bb1b064e241db52170eb2e0d99856f7
seaglasses14 <luca.degennaro0302@gmail.com>
2026-03-09 18:30:28 +0100

    Adde DoorLever and SlidingDoor
    - Fixed ProximityDoor
    - Fixed PressurePlate

---
+++ Source/ChronoSwitch/Private/Gameplay/GameplayActors/DoorLever.cpp
@@ -0,0 +1,26 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Gameplay/GameplayActors/DoorLever.h"
+
+
+// Sets default values
+ADoorLever::ADoorLever()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+// Called when the game starts or when spawned
+void ADoorLever::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ADoorLever::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
