e4ad37dfe61d9615ded7ad35c1953a5704b8a62f
Nicolas <nicolas.mancone11@gmail.com>
2026-03-11 22:32:02 +0100

    Import Character and watch, started StreamLevelLoader

---
+++ Source/ChronoSwitch/Private/Gameplay/World/StreamLevelLoader.cpp
@@ -0,0 +1,80 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Gameplay/World/StreamLevelLoader.h"
+#include "Components/BoxComponent.h"
+#include "Kismet/GameplayStatics.h"
+#include "GameFramework/Character.h"
+
+AStreamLevelLoader::AStreamLevelLoader()
+{
+	PrimaryActorTick.bCanEverTick = false;
+
+	TriggerZone = CreateDefaultSubobject<UBoxComponent>(TEXT("TriggerZone"));
+	RootComponent = TriggerZone;
+	TriggerZone->SetCollisionProfileName(TEXT("Trigger"));
+}
+
+void AStreamLevelLoader::BeginPlay()
+{
+	Super::BeginPlay();
+	TriggerZone->OnComponentBeginOverlap.AddDynamic(this, &AStreamLevelLoader::OnOverlapBegin);
+}
+
+void AStreamLevelLoader::OnOverlapBegin(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	if (OtherActor && OtherActor->IsA(ACharacter::StaticClass()) && HasAuthority())
+	{
+		// TODO: Check if ALL players are inside before starting.
+		StartLevelTransition();
+	}
+}
+
+void AStreamLevelLoader::StartLevelTransition()
+{
+	UE_LOG(LogTemp, Warning, TEXT("Loader: Transition Starting..."));
+
+	// Unload Current Level
+	if (!CurrentLevel.IsNull())
+	{
+		FName LevelName = FName(*CurrentLevel.ToSoftObjectPath().GetLongPackageName());
+		FLatentActionInfo LatentInfo = GetLatentAction(0, "OnLevelUnloaded");
+		UGameplayStatics::UnloadStreamLevel(this, LevelName, LatentInfo, false);
+	}
+	else
+	{
+		// No level to unload, go straight to loading
+		OnLevelUnloaded();
+	}
+}
+
+void AStreamLevelLoader::OnLevelUnloaded()
+{
+	UE_LOG(LogTemp, Warning, TEXT("Loader: Level Unloaded. Loading Next..."));
+
+	// Load Next Level
+	if (!NextLevel.IsNull())
+	{
+		FName LevelName = FName(*NextLevel.ToSoftObjectPath().GetLongPackageName());
+		FLatentActionInfo LatentInfo = GetLatentAction(1, "OnLevelLoaded");
+		UGameplayStatics::LoadStreamLevel(this, LevelName, true, true, LatentInfo);
+	}
+}
+
+void AStreamLevelLoader::OnLevelLoaded()
+{
+	UE_LOG(LogTemp, Warning, TEXT("Loader: Next Level Ready."));
+	
+	// Update References 
+	CurrentLevel = NextLevel;
+	NextLevel = nullptr; 
+}
+
+FLatentActionInfo AStreamLevelLoader::GetLatentAction(int32 ID, FName FunctionName)
+{
+	FLatentActionInfo Info;
+	Info.CallbackTarget = this;
+	Info.ExecutionFunction = FunctionName;
+	Info.Linkage = ID;
+	Info.UUID = ID; // Unique ID for the latent action manager
+	return Info;
+}
---
+++ Source/ChronoSwitch/Public/Gameplay/World/StreamLevelLoader.h
@@ -0,0 +1,51 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "StreamLevelLoader.generated.h"
+
+class UBoxComponent;
+
+/**
+ * A persistent actor that handles streaming levels in/out based on player overlap.
+ */
+UCLASS()
+class CHRONOSWITCH_API AStreamLevelLoader : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	AStreamLevelLoader();
+
+	// The level currently loaded (to be unloaded)
+	UPROPERTY(EditInstanceOnly, Category = "Level Streaming")
+	TSoftObjectPtr<UWorld> CurrentLevel;
+
+	// The level to load next
+	UPROPERTY(EditInstanceOnly, Category = "Level Streaming")
+	TSoftObjectPtr<UWorld> NextLevel;
+
+	UPROPERTY(VisibleAnywhere, Category = "Components")
+	TObjectPtr<UBoxComponent> TriggerZone;
+
+protected:
+	virtual void BeginPlay() override;
+
+	UFUNCTION()
+	void OnOverlapBegin(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
+	// Unload current level
+	void StartLevelTransition();
+
+	// Load next level 
+	UFUNCTION()
+	void OnLevelUnloaded();
+
+	// Finalize 
+	UFUNCTION()
+	void OnLevelLoaded();
+
+	FLatentActionInfo GetLatentAction(int32 ID, FName FunctionName);
+};
